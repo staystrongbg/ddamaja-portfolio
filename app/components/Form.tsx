f1e59b32240dc88e63d4f5bc19e73af3c12842c4
ddamaja <staystrongbg@gmail.com>
2023-11-26 13:21:38 +0100

    emailjs instead resend

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -3,35 +3,60 @@
 import { useRef } from 'react';
 import { sendEmail } from '../lib/actions';
 import Button from './Button';
-
+import emailjs from '@emailjs/browser';
 const Form = () => {
   const formRef = useRef<HTMLFormElement>(null);
+  const resetForm = () => {
+    formRef.current?.reset();
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formRef.current?.elements);
+
+    emailjs
+      .sendForm(
+        'service_zzd160n',
+        'template_v9zm3r5',
+        formRef.current as HTMLFormElement,
+        'user_c6RYBq55w7YHC8Ig0xPNf'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    resetForm();
+  };
   return (
     <form
       ref={formRef}
-      action={async (formData) => {
-        formRef.current?.reset();
-        await sendEmail(formData);
-      }}
+      // action={async (formData) => {
+      //   formRef.current?.reset();
+      //   await sendEmail(formData);
+      // }}
+      onSubmit={handleSubmit}
       className="flex flex-col gap-4 p-4 w-[600px] h-fit border border-purple-200  dark:border-purple-900 rounded-lg shadow-lg
 "
     >
       <input
         type="text"
         placeholder="Subject"
-        name="subject"
+        name="user_subject"
         className="px-4 py-2  border-b border-purple-200   focus:bg-purple-950/10 dark:border-purple-900 bg-transparent outline-none placeholder:text-gray-700 dark:text-fuchsia-400"
       />
       <input
         type="text"
         placeholder="Name"
-        name="name"
+        name="user_name"
         className="px-4 py-2   border-b border-purple-200  focus:bg-purple-950/10 dark:border-purple-900 bg-transparent outline-none placeholder:text-gray-700 dark:text-fuchsia-400"
       />
       <input
         type="email"
         placeholder="Email"
-        name="email"
+        name="user_email"
         className="px-4 py-2   border-b border-purple-200  focus:bg-purple-950/10 dark:border-purple-900 bg-transparent outline-none placeholder:text-gray-700 dark:text-fuchsia-400"
       />
       <textarea
